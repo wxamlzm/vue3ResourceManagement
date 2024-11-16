fa5490ee9d78345d50920ee50e731f51eeda2b32
wxamlzm <zd5024@163.com>
2024-11-16 21:13:40 +0800

    chore: 全局引入

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import './assets/main.css'
+// 引入全局样式
+import 'ant-design-vue/dist/reset.css'; 
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 引入 ant-design-vue
+import Antd from 'ant-design-vue';
 
 const app = createApp(App)
 
+// 使用 ant-design-vue
+app.use(Antd);
 app.use(createPinia())
 app.use(router)
 
